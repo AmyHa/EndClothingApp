410c8d247ace0f0dd02c11d282f153705eb73653
Amy Ha <>
2022-06-02 09:55:45 +0100

    initial commit – app skeleton

---
+++ EndClothingApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  EndClothingApp
+//
+//  Created by Amy Ha on 02/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
