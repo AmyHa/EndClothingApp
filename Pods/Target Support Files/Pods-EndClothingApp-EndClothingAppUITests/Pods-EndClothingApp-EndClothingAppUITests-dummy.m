8695d470fa09758921862c3f7cd1b5e2c25b0847
Amy Ha <>
2022-06-12 14:47:30 +0100

    Committing Pod files for SDWebImage (easier for the reviewer to run the project)

---
+++ Pods/Target Support Files/Pods-EndClothingApp-EndClothingAppUITests/Pods-EndClothingApp-EndClothingAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EndClothingApp_EndClothingAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_EndClothingApp_EndClothingAppUITests
+@end
---
+++ Pods/Target Support Files/Pods-EndClothingApp/Pods-EndClothingApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EndClothingApp : NSObject
+@end
+@implementation PodsDummy_Pods_EndClothingApp
+@end
