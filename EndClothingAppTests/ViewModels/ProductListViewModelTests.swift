1611806d383a07d9199df1c2e5810ad06b8bbb98
Amy Ha <>
2022-06-13 23:39:26 +0100

    Remove incomplete unit test(s)

--- EndClothingAppTests/ViewModels/ProductListViewModelTests.swift
+++ EndClothingAppTests/ViewModels/ProductListViewModelTests.swift
@@ -15,42 +15,8 @@ class ProductListViewModelTests: XCTestCase {
     
     override func setUpWithError() throws {
         viewModel = ProductListViewModel()
-        let configuration = URLSessionConfiguration.ephemeral
-        configuration.protocolClasses = [MockURLProtocol.self]
-        urlSession = URLSession(configuration: configuration)
-        viewModel.service = NetworkService(session: urlSession)
     }
 
     override func tearDownWithError() throws {
     }
-    
-//    func test_fetchProducts_shouldSetProductsArray() {
-//        let mockData = [Product(id: "", name: "Test shirt", price: "£199", image: "an-image-url.com")]
-//
-//        do {
-//            let encodedMockData = try JSONEncoder().encode(mockData)
-//            MockURLProtocol.requestHandler = { request in
-//                return (URLResponse(), encodedMockData)
-//            }
-//        } catch {
-//            XCTFail("Failed to encode mock data.")
-//        }
-//
-//        let expectation = XCTestExpectation(description: "response")
-//
-//        viewModel.service.fetchData(with: "") { (result: Result<Products, Error>) in
-//            switch result {
-//            case .success(let success):
-//                self.viewModel.products = success.products
-//            case .failure(let failure):
-//                XCTFail("Failure")
-//            }
-//            expectation.fulfill()
-//        }
-//
-//        
-//        wait(for: [expectation], timeout: 3)
-//
-//        XCTAssertEqual(viewModel.products, mockData)
-//    }
 }
