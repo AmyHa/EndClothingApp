05c4481aa23da2687d45e757b40d7db8ea30c636
Amy Ha <>
2022-06-13 18:52:58 +0100

    Updating colours of the tab bar items: from default system blue to black when selected

--- EndClothingApp/SceneDelegate.swift
+++ EndClothingApp/SceneDelegate.swift
@@ -24,13 +24,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let accountViewController = ProductListViewController()
         let bagViewController = ProductListViewController()
         
-        let searchItem = UITabBarItem(title: "", image: UIImage(systemName: "doc.text.magnifyingglass"), selectedImage: UIImage(systemName: "doc.text.magnifyingglass"))
-        let savedItem = UITabBarItem(title: "", image: UIImage(systemName: "bookmark"), selectedImage: UIImage(systemName: "bookmark"))
+        let searchItem = UITabBarItem(title: "", image: UIImage(systemName: "doc.text.magnifyingglass"), selectedImage: UIImage(systemName: "doc.text.magnifyingglass")?.withRenderingMode(.alwaysOriginal))
+        
+        // For a strange reason, the bookmark symbol is red by default when selected, so I had to specify it to be black
+        let savedItem = UITabBarItem(title: "", image: UIImage(systemName: "bookmark"), selectedImage: UIImage(systemName: "bookmark")?.withTintColor(UIColor.black).withRenderingMode(.alwaysOriginal))
         
         // Note: in the task app design, the middle tab bar item looks like it is an image of the logo "END.", which I can't find a reasonable replacement for, so I have used a filled square instead
-        let productListItem = UITabBarItem(title: "", image: UIImage(systemName: "square.fill"), selectedImage: UIImage(systemName: "square.fill"))
-        let accountItem = UITabBarItem(title: "", image: UIImage(systemName: "person.crop.circle"), selectedImage: UIImage(systemName: "person.crop.circle"))
-        let bagItem = UITabBarItem(title: "", image: UIImage(systemName: "bag"), selectedImage: UIImage(systemName: "bag"))
+        let productListItem = UITabBarItem(title: "", image: UIImage(systemName: "square.fill"), selectedImage: UIImage(systemName: "square.fill")?.withRenderingMode(.alwaysOriginal))
+        let accountItem = UITabBarItem(title: "", image: UIImage(systemName: "person.crop.circle"), selectedImage: UIImage(systemName: "person.crop.circle")?.withRenderingMode(.alwaysOriginal))
+        let bagItem = UITabBarItem(title: "", image: UIImage(systemName: "bag"), selectedImage: UIImage(systemName: "bag")?.withRenderingMode(.alwaysOriginal))
         
         searchVewController.tabBarItem = searchItem
         savedViewController.tabBarItem = savedItem
